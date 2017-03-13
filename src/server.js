b04542e5466e5e0d4f851381e443f7968a27a025
CORP\EDUAWU <eduardwu@gmail.com>
2017-03-13 19:49:15 +0100

    Added basic telegram bot

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 var SlackBots = require('slackbots')
+var TelegramBot = require('telegraf')
 var request = require('request')
 
-
-var bot = new SlackBots({
-    token: process.env.TOKEN ,
+var botSlack = new SlackBots({
+    token: process.env.SLACKTOKEN ,
     name: 'General Stavoros'
 });
+
+var botTelegram = new TelegramBot(process.env.TELEGRAMTOKEN);
+
+botTelegram.command('start', (ctx) => {
+  console.log('start', ctx.from)
+  ctx.reply('Welcome!')
+})
+
+botTelegram.hears('hi', (ctx) => ctx.reply('WHAHAHAHAHAHAHA'))
+
+botTelegram.on('sticker', (ctx) => ctx.reply('KEKEKEKEKEKEKEKE'))
+
+botTelegram.startPolling()
