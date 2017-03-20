82ab2833ed65b8fe28368c76d8c91cee52922c01
eduardwu <eduardwu@gmail.com>
2017-03-20 19:38:25 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const API_TOKEN = process.env.TELEGRAMTOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://telegram-gradenko-bot.herokuapp.com';
 
-const botTelegram = new Telegraf(API_TOKEN);
+const botTelegram = new TelegramBot(API_TOKEN);
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
