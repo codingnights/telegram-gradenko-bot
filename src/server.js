18bff6216ce315c6352a0354e33be2ff824eb8af
eduardwu <eduardwu@gmail.com>
2017-03-20 19:49:16 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,29 +1,29 @@
-const TelegramBot = require('telegraf')
+const bot = new telegraf(API_TOKEN);
+const Telegraf = require('telegraf');
 const express = require('express');
 const expressApp = express();
-const request = require('request')
 
-const API_TOKEN = process.env.TELEGRAMTOKEN || '';
+const API_TOKEN = process.env.API_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://telegram-gradenko-bot.herokuapp.com';
 
-const botTelegram = new TelegramBot(API_TOKEN);
+const bot = new Telegraf(API_TOKEN);
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
 
 expressApp.get('/', (req, res) => {
-  res.send('Hello World!');
+//   res.send('Hello World!');
   
-//   res.command('start', (ctx) => {
-//     console.log('start', ctx.from)
-//     ctx.reply('Welcome!')
-//   })
+  res.command('start', (ctx) => {
+    console.log('start', ctx.from)
+    ctx.reply('Welcome!')
+  })
 
-//   res.hears('hi', (ctx) => ctx.reply('WHAHAHAHAHAHAHA'))
+  res.hears('hi', (ctx) => ctx.reply('WHAHAHAHAHAHAHA'))
 
-//   res.on('sticker', (ctx) => ctx.reply('KEKEKEKEKEKEKEKE'))
+  res.on('sticker', (ctx) => ctx.reply('KEKEKEKEKEKEKEKE'))
 
-//   res.startPolling() 
+  res.startPolling() 
 });
 
 expressApp.listen(PORT, () => {
